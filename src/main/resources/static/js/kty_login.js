15be0b61f502a7f2b53ab873df4f32056a67693e
taeyoung0504 <tang050405@gmail.com>
2023-06-14 15:09:18 +0900

    header modify

--- src/main/resources/static/js/kty_login.js
+++ src/main/resources/static/js/kty_login.js
@@ -1,13 +1,15 @@
-$(document).ready(function() {
-  $("#kakao_login_btn").click(function() {
-    location.href = "/oauth2/authorization/kakao";
-  });
+$(function () {
+  $(document).ready(function() {
+    $("#kakao_login_btn").on("click", function() {
+      location.href = "/oauth2/authorization/kakao";
+    });
 
-  $("#google_login_btn").click(function() {
-    location.href = "/oauth2/authorization/google";
-  });
+    $("#google_login_btn").on("click", function() {
+      location.href = "/oauth2/authorization/google";
+    });
 
-  $("#naver_login_btn").click(function() {
-    location.href = "/oauth2/authorization/naver";
+    $("#naver_login_btn").on("click", function() {
+      location.href = "/oauth2/authorization/naver";
+    });
   });
 });
