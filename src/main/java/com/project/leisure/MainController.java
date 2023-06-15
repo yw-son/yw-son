bf28d67f76a0cac693b5a94dc8d4d3615576a7ad
taeyoung0504 <tang050405@gmail.com>
2023-06-15 09:13:01 +0900

    main Controller

--- src/main/java/com/project/leisure/MainController.java
+++ src/main/java/com/project/leisure/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
 	@GetMapping("/")
-	public String root() {
+	public String main() {
 		return "main";
 	}
 }
