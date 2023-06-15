1a47ff852baa56c44c4d5de97eabe7b1da590983
taeyoung0504 <tang050405@gmail.com>
2023-06-15 09:19:21 +0900

    OAuth2 사용자 세션 정보

--- src/main/java/com/project/leisure/taeyoung/email/SessionUser.java
+++ src/main/java/com/project/leisure/taeyoung/email/SessionUser.java
@@ -1,11 +1,14 @@
 package com.project.leisure.taeyoung.email;
 
+
 import java.io.Serializable;
 
 import com.project.leisure.taeyoung.user.Users;
 
 import lombok.Getter;
 
+
+//OAuth2 사용자 세션 정보
 @Getter
 public class SessionUser implements Serializable {
 	private String social_nickname;
