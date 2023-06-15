73a55482774b187fec81780ab1175b4e5123451f
taeyoung0504 <tang050405@gmail.com>
2023-06-15 09:20:21 +0900

    회원가입 입력검증 클래스

--- src/main/java/com/project/leisure/taeyoung/user/UserCreateForm.java
+++ src/main/java/com/project/leisure/taeyoung/user/UserCreateForm.java
@@ -1,5 +1,7 @@
 package com.project.leisure.taeyoung.user;
 
+
+// 회원가입 입력검증 클래스
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
