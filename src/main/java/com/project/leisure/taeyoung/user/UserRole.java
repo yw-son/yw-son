bd061f31d5b28ad857f8294e30fd0acc0af365e4
taeyoung0504 <tang050405@gmail.com>
2023-06-15 09:22:28 +0900

    User 등급 선언

--- src/main/java/com/project/leisure/taeyoung/user/UserRole.java
+++ src/main/java/com/project/leisure/taeyoung/user/UserRole.java
@@ -2,6 +2,8 @@ package com.project.leisure.taeyoung.user;
 
 import lombok.Getter;
 
+//User 등급 선언
+
 @Getter
 public enum UserRole {
 	ADMIN("ROLE_ADMIN"), PARTNER("ROLE_PARTNER"), USER("ROLE_USER"), SNS_USER("ROLE_SNS");
