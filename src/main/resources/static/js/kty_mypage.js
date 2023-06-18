3c08e7d31b53109461d25c22f62c7e6fb49e559d
taeyoung0504 <tang050405@gmail.com>
2023-06-18 18:11:33 +0900

    mypage modify

--- src/main/resources/static/js/kty_mypage.js
+++ src/main/resources/static/js/kty_mypage.js
@@ -1,8 +1,12 @@
 $(function() {
 	$(document).ready(function() {
 		$("#mypage_email_modify").on("click", function() {
-			alert('버튼테스트');
+			var modifyEmailInput = $("#modify_email_input");
+			if (modifyEmailInput.css("display") === "none") {
+				modifyEmailInput.css("display", "block");
+			} else {
+				modifyEmailInput.css("display", "none");
+			}
 		});
-
 	});
 });
