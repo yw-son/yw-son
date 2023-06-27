e9ef3438c01518d69d28312f04f957717f4885fd
taeyoung0504 <tang050405@gmail.com>
2023-06-27 09:45:09 +0900

    mypage css modify

--- src/main/java/com/project/leisure/taeyoung/user/RegRepository.java
+++ src/main/java/com/project/leisure/taeyoung/user/RegRepository.java
@@ -1,7 +1,10 @@
 package com.project.leisure.taeyoung.user;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface RegRepository extends JpaRepository<RegPartner, Long> {
-
+	
 }
